f862a427378bba6d2c39469ca983737be7f90c8a
Jack Yu <jackchungchiehyu@gmail.com>
2022-01-04 23:05:59 +0800

    Store asset ids, sorted, in ASSET_IDS

--- spacetimeformer/data/crypto/config.py
+++ spacetimeformer/data/crypto/config.py
@@ -1,6 +1,11 @@
 
 
+import numpy as np
+import pandas as pd
+
+
 DIR_BASE = '/kaggle/input/g-research-crypto-forecasting/'
 
-
+df = pd.read_csv(f'{DIR_BASE}/asset_details.csv')
+ASSET_IDS = sorted(df['Asset_ID'].unique())
 
