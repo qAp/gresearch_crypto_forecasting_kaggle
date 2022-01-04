716ebfc03ffaa12f4a952adaa3e4e956e28eb69c
Jack Yu <jackchungchiehyu@gmail.com>
2022-01-04 23:10:20 +0800

    Store directory path for data preprocessing output

--- spacetimeformer/data/crypto/config.py
+++ spacetimeformer/data/crypto/config.py
@@ -4,7 +4,8 @@ import numpy as np
 import pandas as pd
 
 
-DIR_BASE = '/kaggle/input/g-research-crypto-forecasting/'
+DIR_BASE = '/kaggle/input/g-research-crypto-forecasting'
+DIR_PREPROCESS = '/kaggle/input/stfa01-data-preprocessing'
 
 df = pd.read_csv(f'{DIR_BASE}/asset_details.csv')
 ASSET_IDS = sorted(df['Asset_ID'].unique())
