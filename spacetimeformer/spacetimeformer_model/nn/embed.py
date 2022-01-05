bbe1c0461bb0561047b22b925d3caf667cff03ec
Jack Yu <jackchungchiehyu@gmail.com>
2022-01-05 23:39:09 +0800

    Revert "DBG. Print d_y in SpacetimeformerEmbedding"
    This reverts commit 1d07cc94a885c9f562da098c2f0077591011ed96.

--- spacetimeformer/spacetimeformer_model/nn/embed.py
+++ spacetimeformer/spacetimeformer_model/nn/embed.py
@@ -37,7 +37,6 @@ class SpacetimeformerEmbedding(nn.Module):
         self.y_emb = nn.Linear(y_emb_inp_dim, d_model)
 
         if self.method == "spatio-temporal":
-            print(f'__init__ d_y {d_y}')
             self.var_emb = nn.Embedding(num_embeddings=d_y, embedding_dim=d_model)
 
         self.start_token_len = start_token_len
@@ -122,7 +121,7 @@ class SpacetimeformerEmbedding(nn.Module):
 
     def parallel_spatio_temporal_embed(self, y, x, is_encoder=True):
         bs, length, d_y = y.shape
-        print(f'parallel_spatio_temporal_embed d_y {d_y}')
+
         # val  + time embedding
         y = torch.cat(y.chunk(d_y, dim=-1), dim=1)
         local_pos = (
