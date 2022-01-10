c0770c023da4a79f6e2d6d4b9abeed3eae3dc0b5
Jack Yu <jackchungchiehyu@gmail.com>
2022-01-11 04:56:38 +0800

    Define features list in config
    These are features each asset is expected to have, excluding the
    timestamp.

--- spacetimeformer/data/crypto/config.py
+++ spacetimeformer/data/crypto/config.py
@@ -7,6 +7,9 @@ import pandas as pd
 DIR_BASE = '/kaggle/input/g-research-crypto-forecasting'
 DIR_PREPROCESS = '/kaggle/input/stfa01-data-preprocessing'
 
+FEATURES = ['Count', 'Open', 'High', 'Low', 'Close', 'Volume', 'VWAP', 
+            'Target']
+
 df = pd.read_csv(f'{DIR_BASE}/asset_details.csv')
 ASSET_IDS = sorted(df['Asset_ID'].unique())
 
