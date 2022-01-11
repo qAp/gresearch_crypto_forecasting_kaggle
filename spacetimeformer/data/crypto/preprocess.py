c5f0b43fd6a27c282851d993d769480089f0bfc4
Jack Yu <jackchungchiehyu@gmail.com>
2022-01-11 09:04:27 +0800

    Update preprocessing steps
    1. Make train/valid split.
    2. Compute basic stats of the train split.
    3. Deal with inf in VWAP_10.

--- spacetimeformer/data/crypto/preprocess.py
+++ spacetimeformer/data/crypto/preprocess.py
@@ -3,7 +3,7 @@ import os, sys
 import numpy as np
 import pandas as pd
 
-from spacetimeformer.data.crypto.config import DIR_BASE
+from spacetimeformer.data.crypto.config import DIR_BASE, ASSET_IDS, FEATURES
 from spacetimeformer.data.crypto.utils import unstack_assetid
 
 
@@ -11,15 +11,29 @@ train = pd.read_csv(f'{DIR_BASE}/train.csv')
 
 train.to_feather('/kaggle/working/train.feather')
 
+# Make each row a unique timestamp
 df = unstack_assetid(train)
 
-dt = pd.to_datetime(df['timestamp'], unit='s')
-dt = dt.apply(lambda x: str(x))
-dt = dt.str[:-3]  # "%Y-%m-%d %H:%M"
-df['Datetime'] = dt.copy()
-df.drop('timestamp', axis=1, inplace=True)
+df = df.sort_values('timestamp', axis=0)
+val_pct = .2
+val_cut = int((1 - val_pct) * len(df))
+df.loc[df.index < val_cut, 'split'] = 'train'
+df.loc[df.index >= val_cut, 'split'] = 'valid'
 
 fn = '/kaggle/working/train_tindex.feather'
 df.to_feather(fn)
+
+feature_cols = [f'{feature}_{id}' for id in ASSET_IDS for feature in FEATURES]
+
+is_train = df['split'] == 'train'
+
+stats_train = df[feature_cols][is_train].describe()
+
+# VWAP_10 has `inf` values. Removing these and recalculate basic stats
+srs = df.loc[is_train, 'VWAP_10']
+srs = srs[(- np.inf < srs) & (srs < np.inf)]
+stats_train.loc[:, 'VWAP_10'] = srs.describe()
+
+stats_train.to_csv('/kaggle/working/stats_train.csv', index=False)
 
 
