1cb8e535f80a166885b76c263ff1acac5f69a14f
Jack Yu <jackchungchiehyu@gmail.com>
2022-01-13 23:45:45 +0800

    Define features related to timestamps

--- spacetimeformer/data/crypto/config.py
+++ spacetimeformer/data/crypto/config.py
@@ -10,6 +10,8 @@ DIR_PREPROCESS = '/kaggle/input/stfa01-data-preprocessing'
 FEATURES = ['Count', 'Open', 'High', 'Low', 'Close', 'Volume', 'VWAP', 
             'Target']
 
+TIME_FEATURES = ['Year', 'Month', 'Day', 'Weekday', 'Hour', 'Minute']
+
 df = pd.read_csv(f'{DIR_BASE}/asset_details.csv')
 ASSET_IDS = sorted(df['Asset_ID'].unique())
 
