df57d41d0b919eed22c37cba7d2836b555b050b9
Jack Yu <jackchungchiehyu@gmail.com>
2022-01-30 18:11:09 +0800

    Keep record of prediction and ground truth
    in the submission simulator.

--- spacetimeformer/data/crypto/sim_env.py
+++ spacetimeformer/data/crypto/sim_env.py
@@ -6,7 +6,6 @@ import pandas as pd
 class SimEnv:
     def __init__(self, csv_path='train.csv', num_sample=None):
         self.df = pd.read_csv(csv_path)
-        self.df.drop('Target', axis=1, inplace=True)
 
         timestamps = self.df['timestamp'].unique()
 
@@ -17,10 +16,12 @@ class SimEnv:
         self.num_sample = num_sample
         self.timestamps = timestamps[-num_sample:]
 
-        self._row_count = 0
+        self.reset()
 
     def reset(self):
         self._row_count = 0
+        self.gt_df_list = []
+        self.pr_df_list = []
 
     def iter_test(self):
         for i, timestamp in enumerate(self.timestamps):
@@ -30,12 +31,20 @@ class SimEnv:
             row_id = self._row_count + np.arange(len(test_df))
             test_df['row_id'] = row_id
 
+            gt_df = test_df[['timestamp', 'Asset_ID', 'Target', 'row_id']].copy()
+            self.gt_df_list.append(gt_df)
+
+            test_df.drop('Target', axis=1, inplace=True)
+
             sample_prediction_df = pd.DataFrame({'row_id': row_id})
             sample_prediction_df['Target'] = 0
 
             self._row_count += len(test_df)
 
             yield test_df, sample_prediction_df
+
+    def predict(self, sample_prediction_df):
+        self.pr_df_list.append(sample_prediction_df)
 
     def __len__(self):
         return self.num_sample
