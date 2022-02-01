633301ecb7da886e902e46aad69651909314a6d4
Jack Yu <jackchungchiehyu@gmail.com>
2022-02-01 18:18:42 +0800

    Remove beetcoin
    This was not part of the final solution.

--- beetcoin/config.py
+++
@@ -1,6 +0,0 @@
-
-from beetcoin.utils import initialize_device
-
-DEVICE = 'TPU'
-
-DEVICE, AUTO, REPLICAS = initialize_device(device=DEVICE)
--- beetcoin/data/utils.py
+++
@@ -1,5 +0,0 @@
-import datatable
-
-
-def read_jay(pth):
-    return datatable.fread(pth).to_pandas()
